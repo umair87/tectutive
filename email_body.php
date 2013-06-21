4b337fbe1594c3ab7826afc9e2457be0db0d18f2
umair87 <umair_kureshi@hotmail.com>
2013-06-21 11:57:16 +0500

    Create email_body.php

---
+++ email_body.php
@@ -0,0 +1,56 @@
+<div style="width:597px; height:862px;">
+<div><img src="<?php echo $url;?>/top.jpg" width="596" height="74" /></div>
+<div style="position:relative;">
+<div style="position:absolute; bottom:58px; right:50px;"><a href="#"><img src="<?php echo $url;?>/send-credit-small.jpg" width="115" height="24" border="0" /></a></div>
+<div><img src="<?php echo $url;?>/about.jpg" width="596" height="541" /></div>
+
+</div>
+<div style="height:90px; background-color:#fdcf35; margin-bottom:10px; float:left; width:597px;">
+<div style="float:left; margin-left:20px; margin-right:20px; padding-top:15px; width:100px;text-align:center;">
+<h1 style="font-family: 'dandelion'; font-size:18px; margin:0px; padding:0px; color:#fff;">PAKISTAN</h1>
+
+<div style="padding-top:10px;"><img src="<?php echo $url;?>/mobitop-emailer-FINAL_05.jpg" width="47" height="28" /></div>
+
+</div>
+<div style="width:250px; float:left; padding-top:8px;">
+<p style="margin:0px; padding:0px; font-size:15px; font-weight:bold; font-family:Arial, Helvetica, sans-serif; color:#616465">PROMO 1</p>
+<p style="margin:0px; padding:0px; font-size:12px; font-weight:normal; font-family:Arial, Helvetica, sans-serif; color:#616465">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue aliquet aliquam Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+</div>
+<div style="float:left; margin-top:35px;"><a href="#"><img src="<?php echo $url;?>/send-credit-now.jpg" width="176" height="32" border="0" /></a></div>
+
+
+</div>
+<div style="height:90px; background-color:#fdcf35; margin-bottom:10px; float:left; width:597px;">
+<div style="float:left; margin-left:20px; margin-right:20px; padding-top:15px; width:100px; text-align:center;">
+<h1 style="font-family: 'dandelion'; font-size:18px; margin:0px; padding:0px; color:#fff;">SINGAPORE</h1>
+<div style="padding-top:10px;"><img src="<?php echo $url;?>/singapore.jpg" width="47" height="28" /></div>
+
+</div>
+<div style="width:250px; float:left; padding-top:8px;">
+<p style="margin:0px; padding:0px; font-size:15px; font-weight:bold; font-family:Arial, Helvetica, sans-serif; color:#616465">PROMO 1</p>
+<p style="margin:0px; padding:0px; font-size:12px; font-weight:normal; font-family:Arial, Helvetica, sans-serif; color:#616465">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue aliquet aliquam Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+</div>
+<div style="float:left; margin-top:35px;"><a href="#"><img src="<?php echo $url;?>/send-credit-now.jpg" width="176" height="32" border="0" /></a></div>
+</div>
+
+<div style="float:left; margin-left:50px; margin-top:10px;"><table width="94" border="0" cellpadding="0" cellspacing="0">
+  <tr>
+    <td width="16"><a href="#"><img src="<?php echo $url;?>/f-icon.jpg" width="17" height="24" border="0" /></a></td>
+    <td width="16"><a href="#"><img src="<?php echo $url;?>/rss.jpg" width="20" height="24" border="0" /></a></td>
+    <td width="16"><a href="#"><img src="<?php echo $url;?>/you-tube.jpg" width="20" height="24" border="0" /></a></td>
+    <td width="18"><a href="#"><img src="<?php echo $url;?>/t-icon.jpg" width="20" height="24" /></a></td>
+  </tr>
+</table>
+</div>
+
+<div style="margin-top:10px; margin-left:60px; float:left"><a href="#"><img src="<?php echo $url;?>/contact-img.jpg" width="68" height="18" border="0" /></a></div>
+
+<div style="float:right; margin-right:0px;">
+<div style="float:left; margin-right:10px;"><a href="#"><img src="<?php echo $url;?>/download_android.png" width="116" height="38" border="0" /></a></div>
+<div style="float:left; margin-right:10px;"><a href="#"><img src="<?php echo $url;?>/download_blackberry.png" width="116" height="38" border="0" /></a></div>
+
+</div>
+
+</div>
